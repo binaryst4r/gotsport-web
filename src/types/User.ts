e02a0528ab670ca607de29c0267c56221ce0a53a
Larry Gust <larry.v.gust@gmail.com>
2022-09-26 11:56:59 -0700

    Auth context and User type

---
+++ src/types/User.ts
@@ -0,0 +1,42 @@
+export interface User {
+  address1: String
+  address2: String
+  allergies: String
+  alternate_email: String
+  appearance_template_id: Number
+  authentication_token: String
+  authentication_token_sent_at: String
+  authentication_token_validated_at: String
+  avatar_data: String
+  birth_certificate_content_type: String
+  birth_certificate_file_name: String
+  birth_certificate_file_size: Number
+  birth_certificate_status: String
+  birth_certificate_updated_at: String
+  birth_certificate_verified_at: String
+  birth_certificate_verifying_organization_id: Number
+  birth_certificate_verifying_user_id: Number
+  birthdate: String
+  city: String
+  club_id: String
+  country: String
+  created_at: String
+  current_role_id: String 
+  current_sign_in_ip: String
+  date_format: String
+  email: String
+  email_locked: Boolean
+  ethnicity: String
+  excel_codes: String
+  favorite_club: String
+  fifa_id: String
+  first_name: String
+  last_name: String
+  full_org_email_opt_out: Boolean
+  gender: String
+  gpa: Number
+  grad_year: Number
+  import_referee_tag: String
+  slug: String
+// just gonna add them as I need them. fuck having all of these fields that are likely unnecessary.
+}
